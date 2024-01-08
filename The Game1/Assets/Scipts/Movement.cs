7ae55cb58e4d86cec8503771beccf4a7e25d9f01
DelusionalJelly <155850555+DelusionalJelly@users.noreply.github.com>
2024-01-08 11:17:45 -0700

    Code
    Changed the Code in PlayerMovement to make the player face the mouse cursor

--- The Game1/Assets/Scipts/Movement.cs
+++ The Game1/Assets/Scipts/Movement.cs
@@ -8,14 +8,19 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 moveDirection;
 
+    private Transform Trans;
 
-
+    private void Start()
+    {
+        Trans = this.transform;
+    }
     private void Update()
     {
         ProcessInputs();
     }
     private void FixedUpdate()
     {
+        LookAtMouse();
         Move();
     }
 
@@ -32,4 +37,11 @@ public class Movement : MonoBehaviour
         rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
     }
 
+    void LookAtMouse()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Trans.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        Trans.rotation = rotation;
+    }
 }
